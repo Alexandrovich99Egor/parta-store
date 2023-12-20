494eccb1cbf27fea24514913743ba902c275898b
Egor <129444652+Alexandrovich99Egor@users.noreply.github.com>
2023-12-20 11:13:53 +0200

    del
    del

--- boxshop/functions.php
+++
@@ -1,7 +0,0 @@
-<?php 
-/*** Options Framework ***/
-require_once get_template_directory().'/admin/index.php';
-
-/*** Include Framework File ***/
-require_once get_template_directory().'/framework/init.php';	
-?>
